bb2473f052dab718daa0ebc36577ce2d674f7ed4
Jared Crimmins <jared@crimmins.io>
2020-10-11 14:45:26 -0500

    feat(icon-button): add mdc-icon-button package

---
+++ packages/mdc-icon-button/component.vue
@@ -0,0 +1,47 @@
+<template>
+    <button
+        class="mdc-icon-button material-icons"
+        :disabled="disabled"
+    >
+        <div class="mdc-button__icon">
+            <slot></slot>
+        </div>
+        <!-- <img class="mdc-icon-button__icon" :src="src"> -->
+    </button>
+</template>
+
+<script>
+    "use-strict"
+
+    import { MDCRipple } from "@material/ripple";
+
+    export default {
+        name: "mdc-icon-button",
+
+        props: {
+            disabled: Boolean
+        },
+
+        data() {
+            return {
+                mdcRipple: null
+            }
+        },
+
+        mounted() {
+            this.init();
+        },
+
+        methods: {
+            init() {
+                this.mdcRipple = new MDCRipple(this.$el);
+                this.mdcRipple.unbounded = true;
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    @use "@material/icon-button/mdc-icon-button";
+    @import "https://fonts.googleapis.com/icon?family=Material+Icons";
+</style>
