6024371d242acdae1852c11876cdaa868ccd79e3
Jared Crimmins <crimminsjared@gmail.com>
2020-11-28 10:01:02 -0600

    New folder layout.

---
+++ src/components/mdc-switch/component.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="mdc-switch">
+        <div class="mdc-switch__track"></div>
+        <div class="mdc-switch__thumb-underlay">
+            <div class="mdc-switch__thumb"></div>
+            <input
+                type="checkbox"
+                class="mdc-switch__native-control"
+                role="switch"
+                v-model="checked"
+            >
+        </div>
+    </div>
+</template>
+
+<script>
+    "use-strict"
+
+    import { MDCSwitch } from "@material/switch";
+
+    export default {
+        name: "ToggleSwitch",
+        data: function() {
+            return {
+                checked: false
+            }
+        },
+        mounted: function() {
+            this.mdcTextField = new MDCSwitch(this.$el);
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @use "@material/switch/mdc-switch";
+</style>
