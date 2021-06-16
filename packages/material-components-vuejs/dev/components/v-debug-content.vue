c3463738508a9393e513d133d10f0142cf61ba88
Jared Crimmins <jared@crimmins.io>
2021-06-16 16:50:51 -0500

    chore(dev): create `VDebugContent` component

---
+++ packages/material-components-vuejs/dev/components/v-debug-content.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="v-debug-content">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "v-debug-content"
+  }
+</script>
+
+<style lang="scss">
+  .v-debug-content {
+    width: 500px;
+  }
+</style>
