b875f418bd720651e0529eb6318c0681e9fe93a0
Jared Crimmins <jared@crimmins.io>
2021-06-16 16:50:51 -0500

    feat(menu): create multiple subcomponents
    Create subcomponents `MDCMenuAnchor`, `MDCMenuDivider`, `MDCMenuItem`,
    and `MDCMenuSelectionGroup`.

---
+++ packages/material-components-vuejs/src/components/mdc-menu/mdc-menu-selection-group.js
@@ -0,0 +1,34 @@
+import {cssClasses} from "@material/menu";
+
+export default {
+  name: "mdc-selection-group",
+
+  props: {
+    icon: {
+      type: String,
+      default: "check"
+    }
+  },
+
+  provide() {
+    return {
+      menuSelectionGroup: true,
+      menuSelectionGroupIcon: this.icon
+    };
+  },
+
+  render(c) {
+    return c(
+      "li",
+      [
+        c(
+          "ul",
+          {
+            staticClass: cssClasses.MENU_SELECTION_GROUP
+          },
+          this.$slots.default
+        )
+      ]
+    );
+  }
+}
