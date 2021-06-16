03ba1e70ff6025e2fb4c42e68ef540870bf88d1c
Jared Crimmins <jared@crimmins.io>
2021-06-16 16:50:51 -0500

    chore(dev): create and implement debug components for MCV components

---
+++ packages/material-components-vuejs/dev/components/the-debug-mdc-button.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2 id="mdc-button">MDCButton</h2>
+
+    <mdc-button>MDCButton</mdc-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "the-debug-mdc-button"
+  }
+</script>
+
+<style lang="scss">
+  @use "@material/button/_index.scss" as button;
+
+  @include button.core-styles;
+</style>
---
+++ packages/material-components-vuejs/dev/components/the-debug-mdc-linear-progress.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2 id="mdc-linear-progress">MDCLinearProgress</h2>
+
+    <mdc-linear-progress :open="open"></mdc-linear-progress>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "the-debug-mdc-linear-progress",
+
+    data() {
+      return {
+        open: true
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+  @use "@material/linear-progress/_index.scss" as linear-progress;
+
+  @include linear-progress.core-styles;
+</style>
