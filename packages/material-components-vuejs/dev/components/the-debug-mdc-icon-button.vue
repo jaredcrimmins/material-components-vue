801c752e08448455693e6920eb5197f1b2f10f7b
Jared Crimmins <jared@crimmins.io>
2021-06-16 16:50:51 -0500

    chore(dev): create and implement debug components for MCV components

---
+++ packages/material-components-vuejs/dev/components/the-debug-mdc-icon-button.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2 id="mdc-icon-button">MDCIconButton</h2>
+
+    <mdc-icon-button
+      icon-theme="two-tone"
+      icon="favorite_border"
+      on-icon="favorite"
+      toggleable
+    >
+    </mdc-icon-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "the-debug-mdc-icon-button"
+  }
+</script>
+
+<style lang="scss">
+  @use "@material/icon-button/_index.scss" as icon-button;
+
+  @include icon-button.core-styles;
+</style>
---
+++ packages/material-components-vuejs/dev/components/the-debug-mdc-image-list.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2 id="mdc-image-list">MDCImageList</h2>
+
+    <v-debug-content>
+      <mdc-image-list>
+        <mdc-image-list-item
+          src="https://freesvg.org/storage/zip/blog/Pizza_Pepperoni.svg"
+          label="Hello world!"
+          constrainAspectRatio
+        >
+        </mdc-image-list-item>
+        <mdc-image-list-item
+          src="https://freesvg.org/storage/zip/blog/Hello-World-In-Several-Languages.svg"
+          label="Hello world!"
+          constrainAspectRatio
+        >
+        </mdc-image-list-item>
+        <mdc-image-list-item
+          src="https://freesvg.org/storage/zip/blog/Hello-World-In-Several-Languages.svg"
+          label="Hello world!"
+          constrainAspectRatio
+        >
+        </mdc-image-list-item>
+      </mdc-image-list>
+    </v-debug-content>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "the-debug-mdc-image-list"
+  }
+</script>
+
+<style lang="scss">
+  @use "@material/image-list/_index.scss" as image-list;
+
+  @include image-list.core-styles;
+
+  .mdc-image-list:not(--mdc-image-list--masonry) {
+    @include image-list.standard-columns(2);
+  }
+
+  .mdc-image-list.mdc-image-list--masonry {
+    @include image-list.masonry-columns(2);
+  }
+</style>
