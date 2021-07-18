036132f333d5bfc655aa5b80007ee8bf314363d5
Jared Crimmins <crimminsjared@gmail.com>
2021-07-18 15:33:51 -0500

    fix(snackbar): add default adapter

--- packages/material-components-vuejs/src/components/mdc-snackbar/mdc-snackbar.js
+++ packages/material-components-vuejs/src/components/mdc-snackbar/mdc-snackbar.js
@@ -15,7 +15,9 @@ export default {
 
   data() {
     return {
-      mdcFoundation: null,
+      mdcFoundation: new MDCSnackbarFoundation(
+        MDCSnackbarFoundation.defaultAdapter
+      ),
       open: this.value
     };
   },
