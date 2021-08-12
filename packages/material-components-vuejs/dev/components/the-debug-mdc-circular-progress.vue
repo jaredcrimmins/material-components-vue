86a4a02e0f63adf750baafedc7af8954b86e3460
Jared Crimmins <crimminsjared@gmail.com>
2021-08-12 00:56:32 -0500

    style(dev, line-ripple, select): conform to new ESLint config

--- packages/material-components-vuejs/dev/components/the-debug-mdc-circular-progress.vue
+++ packages/material-components-vuejs/dev/components/the-debug-mdc-circular-progress.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h2 id="mdc-circular-progress">MDCCircularProgress</h2>
+    <h2 id="mdc-circular-progress">
+      MDCCircularProgress
+    </h2>
 
     <h3>Variants</h3>
 
@@ -9,34 +11,40 @@
     <mdc-circular-progress
       size="small"
       :open="smallOpen"
-    ></mdc-circular-progress>
+    />
 
-    <button v-on:click="onToggleSmallButtonClick">Toggle small MDCCircularProgress</button>
+    <button @click="onToggleSmallButtonClick">
+      Toggle small MDCCircularProgress
+    </button>
 
     <h4>Medium</h4>
 
     <mdc-circular-progress
       size="medium"
       :open="mediumOpen"
-    ></mdc-circular-progress>
+    />
 
-    <button v-on:click="onToggleMediumButtonClick">Toggle medium MDCCircularProgress</button>
+    <button @click="onToggleMediumButtonClick">
+      Toggle medium MDCCircularProgress
+    </button>
 
     <h4>Large</h4>
 
     <mdc-circular-progress
       size="large"
       :open="largeOpen"
-    ></mdc-circular-progress>
+    />
 
-    <button v-on:click="onToggleLargeButtonClick">Toggle large MDCCircularProgress</button>
+    <button @click="onToggleLargeButtonClick">
+      Toggle large MDCCircularProgress
+    </button>
 
     <h4>Custom</h4>
 
     <mdc-circular-progress
       size="90px"
       :open="customOpen"
-    ></mdc-circular-progress>
+    />
 
     <h4>Determinate</h4>
     
@@ -45,13 +53,13 @@
       :progress="0.5"
       determinate
       open
-    ></mdc-circular-progress>
+    />
   </div>
 </template>
 
 <script>
   export default {
-    name: "the-debug-mdc-circular-progress",
+    name: "TheDebugMdcCircularProgress",
 
     data() {
       return {
