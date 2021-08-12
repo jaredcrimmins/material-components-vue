dc14c0af93354a623bf855113f55e9b078fe3c5a
Jared Crimmins <jared@crimmins.io>
2021-08-12 01:39:46 -0500

    refactor(line-ripple): convert from .vue to .js file

---
+++ packages/material-components-vuejs/src/components/mdc-line-ripple/mdc-line-ripple.js
@@ -0,0 +1,98 @@
+import {MDCLineRippleFoundation} from "@material/line-ripple";
+
+export default {
+    name: "MdcLineRipple",
+
+    props: {
+        active: Boolean,
+        rippleCenter: {
+            type: Number,
+            default: null
+        }
+    },
+
+    data() {
+        return {
+            mdcFoundation: new MDCLineRippleFoundation(
+                MDCLineRippleFoundation.defaultAdapter
+            )
+        };
+    },
+
+    watch: {
+        active(value) {
+            value ? this.mdcFoundation.activate() : this.mdcFoundation.deactivate();
+        },
+
+        rippleCenter(value) {
+            this.setRippleCenter(value);
+        }
+    },
+
+    mounted() {
+        this.init();
+    },
+
+    beforeDestroy() {
+        this.deinit();
+    },
+
+    render(c) {
+        return c(
+            "span",
+            {
+                class: "mdc-line-ripple"
+            }
+        );
+    },
+
+    methods: {
+        //
+        // Private methods
+        //
+
+        init() {
+            this.mdcFoundation = new MDCLineRippleFoundation(this);
+            this.mdcFoundation.init();
+
+            this.setRippleCenter(this.rippleCenter);
+        },
+
+        deinit() {
+            this.mdcFoundation.destroy();
+        },
+
+        setRippleCenter(rippleCenter) {
+            if (rippleCenter !== null)
+                this.mdcFoundation.setRippleCenter(rippleCenter);
+        },
+
+        //
+        // Adapter methods
+        //
+
+        addClass(className) {
+            this.$el.classList.add(className);
+        },
+
+        removeClass(className) {
+            this.$el.classList.remove(className);
+        },
+
+        hasClass(className) {
+            return this.$el.classList.contains(className);
+        },
+
+        setStyle(propertyName, value) {
+            this.$el.style.setProperty(propertyName, value);
+        },
+
+        registerEventHandler(evtType, handler) {
+            this.$el.addEventListener(evtType, handler);
+        },
+
+        deregisterEventHandler(evtType, handler) {
+            this.$el.removeEventListener(evtType, handler);
+        }
+    }
+}
