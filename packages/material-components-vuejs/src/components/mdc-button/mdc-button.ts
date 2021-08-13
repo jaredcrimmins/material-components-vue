f06c812861e0c6fa2d24297fa356dc2b2a98687e
Jared Crimmins <jared@crimmins.io>
2021-08-13 16:26:12 -0500

    refactor(button,ripple): rewrite components in TypeScript

---
+++ packages/material-components-vuejs/src/components/mdc-button/mdc-button.ts
@@ -0,0 +1,85 @@
+import {MDCRipple} from '../mdc-ripple';
+import Vue, {VueConstructor} from 'vue'
+
+export default Vue.extend({
+  name: 'mdc-button',
+
+  inheritAttrs: true,
+
+  components: {
+    'mdc-ripple': MDCRipple
+  },
+
+  props: {
+    disabled: Boolean,
+    outlined: Boolean,
+    raised: Boolean,
+    rippleDisabled: Boolean,
+    to: String,
+    unelevated: Boolean
+  },
+
+  render(c) {
+    return c(
+      'mdc-ripple',
+      {
+        props: {
+          disabled: this.rippleDisabled,
+          standalone: false
+        },
+        scopedSlots: {
+          default: ({cssClass, on}) => {
+            const tagName = this.to ? 'router-link' : 'button';
+            const isTagNameButton = tagName === 'button';
+
+            return c(
+              tagName,
+              {
+                staticClass: 'mdc-button',
+                class: Object.assign({
+                  'mdc-button--outlined': this.outlined,
+                  'mdc-button--raised': this.raised,
+                  'mdc-button--unelevated': this.unelevated
+                }, cssClass),
+                attrs: {
+                  disabled: isTagNameButton && this.disabled
+                },
+                props: {
+                  to: this.to
+                },
+                on: {
+                  blur: on.blur,
+                  click: this.onClick,
+                  focus: on.focus
+                }
+              },
+              [
+                c(
+                  'div',
+                  {
+                    staticClass: 'mdc-button__ripple'
+                  }
+                ),
+                this.$slots.append,
+                c(
+                  'span',
+                  {
+                    staticClass: 'mdc-button__label'
+                  },
+                  this.$slots.default
+                ),
+                this.$slots.trailing
+              ]
+            );
+          }
+        }
+      }
+    );
+  },
+
+  methods: {
+    onClick(event: MouseEvent) {
+      this.$emit('click', event);
+    }
+  }
+});
