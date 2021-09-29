e210d2a019c7f2d26875de89d4acda259a909e23
Jared Crimmins <jared@crimmins.io>
2021-09-29 15:02:02 -0500

    refactor(floating-label): rewrite in TypeScript

---
+++ packages/material-components-vuejs/src/components/mdc-floating-label/mdc-floating-label.ts
@@ -0,0 +1,122 @@
+import {MDCFloatingLabelFoundation, cssClasses} from '@material/floating-label';
+import {NativeEventListener} from '@/utils';
+import Vue, {VNode} from 'vue';
+import {estimateScrollWidth} from '@material/dom/ponyfill';
+
+export default Vue.extend({
+  name: 'mdc-floating-label',
+
+  inheritAttrs: true,
+
+  props: {
+    content: {
+      default: '',
+      type: String
+    },
+    float: {
+      default: false,
+      type: Boolean
+    },
+    id: String,
+    shake: {
+      default: false,
+      type: Boolean
+    },
+    required: Boolean
+  },
+
+  data() {
+    return {
+      mdcFoundation: new MDCFloatingLabelFoundation(
+        MDCFloatingLabelFoundation.defaultAdapter
+      )
+    };
+  },
+
+  mounted() {
+    this.init();
+  },
+
+  beforeDestory() {
+    this.deinit();
+  },
+
+  watch: {
+    float(value) {
+      this.setFloat(value);
+    },
+
+    shake(value) {
+      this.setShake(value);
+    },
+
+    required(value) {
+      this.setRequired(value);
+    }
+  },
+
+  render(c): VNode {
+    return c(
+      'label',
+      {
+        staticClass: cssClasses.ROOT,
+        attrs: {
+          id: ''
+        }
+      },
+      this.content
+    );
+  },
+
+  methods: {
+    //
+    // Private methods
+    //
+
+    init() {
+      this.mdcFoundation = new MDCFloatingLabelFoundation(this);
+      this.mdcFoundation.init();
+      this.mdcFoundation.setRequired(this.required);
+    },
+
+    deinit() {
+      this.mdcFoundation.destroy();
+    },
+
+    setFloat(shouldFloat: boolean) {
+      this.mdcFoundation.float(shouldFloat);
+    },
+
+    setRequired(isRequired: boolean) {
+      this.mdcFoundation.setRequired(isRequired);
+    },
+
+    setShake(shouldShake: boolean) {
+      this.mdcFoundation.shake(shouldShake);
+    },
+
+    //
+    // Adapter methods
+    //
+
+    addClass(className: string) {
+      this.$el.classList.add(className);
+    },
+
+    removeClass(className: string) {
+      this.$el.classList.remove(className);
+    },
+
+    deregisterInteractionHandler(evtType: string, handler: NativeEventListener) {
+      this.$el.removeEventListener(evtType, handler);
+    },
+
+    getWidth() {
+      return estimateScrollWidth(this.$el);
+    },
+
+    registerInteractionHandler(evtType: string, handler: NativeEventListener) {
+      this.$el.addEventListener(evtType, handler);
+    }
+  }
+});
