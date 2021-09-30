f5d9a6fc3d28a52e6d671744bbac664123de0325
Jared Crimmins <jared@crimmins.io>
2021-09-30 08:36:15 -0500

    refactor(linear-progress): rewrite in TypeScript

---
+++ packages/material-components-vuejs/src/components/mdc-linear-progress/mdc-linear-progress.ts
@@ -0,0 +1,217 @@
+import {MDCLinearProgressFoundation} from "@material/linear-progress";
+import Vue, {CreateElement, VNode} from 'vue';
+
+export default Vue.extend({
+  name: "mdc-linear-progress",
+
+  props: {
+    buffer: {
+      default: 0,
+      type: Number
+    },
+    determinate: {
+      default: false,
+      type: Boolean
+    },
+    open: {
+      default: false,
+      type: Boolean
+    },
+    progress: {
+      default: 0,
+      type: Number,
+      validator(value) {
+        return value >= 0 && value <= 1;
+      }
+    },
+    reverse: {
+      default: false,
+      type: Boolean
+    }
+  },
+
+  data() {
+    return {
+      mdcFoundation: new MDCLinearProgressFoundation(
+        MDCLinearProgressFoundation.defaultAdapter
+      ),
+      bufferBarStyle: <{[key: string]: string}>{},
+      primaryBarStyle: <{[key: string]: string}>{}
+    };
+  },
+
+  watch: {
+    buffer(value) {
+      this.setBuffer(value);
+    },
+
+    determinate(value) {
+      this.setDeterminate(value);
+    },
+
+    open(value) {
+      value ? this.open_() : this.close();
+    },
+
+    progress(value) {
+      this.setProgress(value);
+    },
+
+    reverse(value) {
+      this.setReverse(value);
+    }
+  },
+
+  mounted() {
+    this.init();
+  },
+
+  beforeDestroy() {
+    this.deinit();
+  },
+
+  render(c): VNode {
+    return c(
+      "div",
+      {
+        staticClass: "mdc-linear-progress",
+        attrs: {
+          "aria-valuemax": "1",
+          "aria-valuemin": "0",
+          role: "progress-bar"
+        }
+      },
+      [
+        this.genBuffer(c),
+        this.genPrimaryBar(c),
+        this.genSecondaryBar(c)
+      ]
+    );
+  },
+
+  methods: {
+    //
+    // Private methods
+    //
+
+    init() {
+      this.mdcFoundation = new MDCLinearProgressFoundation(this);
+      this.mdcFoundation.init();
+      this.setDeterminate(this.determinate);
+      this.setProgress(this.progress);
+      this.open ? this.open_() : this.close();
+    },
+
+    deinit() {
+      this.mdcFoundation.destroy();
+    },
+
+    setBuffer(value: number) {
+      this.mdcFoundation.setBuffer(value);
+    },
+
+    setDeterminate(value: boolean) {
+      this.mdcFoundation.setDeterminate(value);
+    },
+
+    setProgress(value: number) {
+      this.mdcFoundation.setProgress(value);
+    },
+
+    setReverse(value) {
+      this.mdcFoundation.setReverse(value);
+    },
+
+    open_() {
+      this.mdcFoundation.open();
+    },
+
+    close() {
+      this.mdcFoundation.close();
+    },
+
+    genBuffer(c: CreateElement) {
+      return c(
+        "div",
+        { staticClass: "mdc-linear-progress__buffer" },
+        [
+          c(
+            "div",
+            {
+              ref: "bufferBarEl",
+              staticClass: "mdc-linear-progress__buffer-bar",
+              style: this.bufferBarStyle
+            }
+          ),
+          c(
+            "div",
+            { staticClass: "mdc-linear-progress__buffer-dots" }
+          )
+        ]
+      );
+    },
+
+    genPrimaryBar(c: CreateElement) {
+      return c(
+        "div",
+        {
+          ref: "primaryBarEl",
+          staticClass: "mdc-linear-progress__bar mdc-linear-progress__primary-bar",
+          style: this.primaryBarStyle
+        },
+        [ this.genBarInner(c) ]
+      );
+    },
+
+    genSecondaryBar(c: CreateElement) {
+      return c(
+        "div",
+        { staticClass: "mdc-linear-progress__bar mdc-linear-progress__secondary-bar" },
+        [ this.genBarInner(c) ]
+      );
+    },
+
+    genBarInner(c: CreateElement) {
+      return c(
+        "span",
+        { staticClass: "mdc-linear-progress__bar-inner" }
+      );
+    },
+
+    //
+    // Adapter methods
+    //
+
+    addClass(className: string) {
+      this.$el.classList.add(className);
+    },
+
+    forceLayout() {
+      this.$el.getBoundingClientRect();
+    },
+
+    setBufferBarStyle(styleProperty: string, value: string) {
+      this.bufferBarStyle[styleProperty] = value;
+    },
+
+    setPrimaryBarStyle(styleProperty: string, value: string) {
+      this.primaryBarStyle[styleProperty] = value;
+    },
+
+    hasClass(className: string) {
+      return this.$el.classList.contains(className);
+    },
+
+    removeAttribute(attributeName: string) {
+      this.$el.removeAttribute(attributeName);
+    },
+
+    removeClass(className: string) {
+      this.$el.classList.remove(className);
+    },
+
+    setAttribute(attributeName: string, value: string) {
+      this.$el.setAttribute(attributeName, value);
+    }
+  }
+});
