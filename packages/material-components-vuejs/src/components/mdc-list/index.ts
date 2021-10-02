13e9353157def969bd6f63524a3466b9ec4fc774
Jared Crimmins <jared@crimmins.io>
2021-10-02 15:12:06 -0500

    fix(switch): fix data attribtue typing error

---
+++ packages/material-components-vuejs/src/components/mdc-list/index.ts
@@ -0,0 +1,4 @@
+export {default as MDCList} from "./mdc-list";
+export {default as MDCListDivider} from "./mdc-list-divider";
+export {default as MDCListGroup} from "./mdc-list-group";
+export {default as MDCListItem} from "./list-item";
