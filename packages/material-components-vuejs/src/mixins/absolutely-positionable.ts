cec5f0bd8101910e75f649c70dc252fd7b58b943
Jared Crimmins <jared@crimmins.io>
2021-10-04 23:04:30 -0500

    refactor(absolutely-positionable): improve `absolutePosition` typing

--- packages/material-components-vuejs/src/mixins/absolutely-positionable.ts
+++ packages/material-components-vuejs/src/mixins/absolutely-positionable.ts
@@ -1,11 +1,12 @@
-import Vue from 'vue';
+import Vue, {PropType} from 'vue';
+import {MDCMenuPoint} from '@material/menu-surface';
 
 export default Vue.extend({
   name: 'absolutely-positionable',
 
   props: {
     absolutePosition: {
-      type: Object,
+      type: <PropType<MDCMenuPoint>>Object,
       validator: value => {
         return Number.isFinite(value.x) && Number.isFinite(value.y);
       }
