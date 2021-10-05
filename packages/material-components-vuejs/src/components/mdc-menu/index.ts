11668a18196ddafd53fabab5cb158dab13e6c240
Jared Crimmins <jared@crimmins.io>
2021-10-04 23:13:21 -0500

    refactor(menu): rewrite in TypeScript

---
+++ packages/material-components-vuejs/src/components/mdc-menu/index.ts
@@ -0,0 +1,5 @@
+export {default as MDCMenu} from "./mdc-menu";
+export {default as MDCMenuAnchor} from "./mdc-menu-anchor";
+export {default as MDCMenuDivider} from "./mdc-menu-divider";
+export {default as MDCMenuItem} from "./mdc-menu-item";
+export {default as MDCMenuSelectionGroup} from "./mdc-menu-selection-group";
---
+++ packages/material-components-vuejs/src/components/mdc-menu/mdc-menu-divider.ts
@@ -0,0 +1,16 @@
+import {MDCListDivider} from "./../";
+import Vue, {VNode} from 'vue';
+
+export default Vue.extend({
+  name: "mdc-menu-divider",
+
+  components: {
+    "mdc-list-divider": MDCListDivider
+  },
+
+  render(c): VNode {
+    return c(
+      "mdc-list-divider"
+    );
+  }
+});
