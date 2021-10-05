621b3529a1b52db01a6c29cc70640948b1e39ad4
Jared Crimmins <jared@crimmins.io>
2021-10-04 23:13:21 -0500

    refactor(menu): rewrite in TypeScript

--- packages/material-components-vuejs/src/components/mdc-menu/index.js
+++
@@ -1,5 +0,0 @@
-export {default as MDCMenu} from "./mdc-menu";
-export {default as MDCMenuAnchor} from "./mdc-menu-anchor";
-export {default as MDCMenuDivider} from "./mdc-menu-divider";
-export {default as MDCMenuItem} from "./mdc-menu-item";
-export {default as MDCMenuSelectionGroup} from "./mdc-menu-selection-group";
