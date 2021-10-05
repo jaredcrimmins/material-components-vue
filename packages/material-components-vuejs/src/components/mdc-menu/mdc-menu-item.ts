759030fb1ea67e3e278164f6a456894bf1dc3bbd
Jared Crimmins <jared@crimmins.io>
2021-10-04 23:13:21 -0500

    refactor(menu): rewrite in TypeScript

---
+++ packages/material-components-vuejs/src/components/mdc-menu/mdc-menu-item.ts
@@ -0,0 +1,31 @@
+import {MDCListItem} from "./../mdc-list";
+import Vue, {VNode} from 'vue';
+
+export default Vue.extend({
+  name: "mdc-menu-item",
+
+  components: {
+    "mdc-list-item": MDCListItem
+  },
+
+  props: {
+    rippleDisabled: Boolean,
+    value: String
+  },
+
+  render(c): VNode {
+    return c(
+      "mdc-list-item",
+      {
+        attrs: {
+          role: "menuitem"
+        },
+        props: {
+          rippleDisabled: this.rippleDisabled,
+          value: this.value
+        }
+      },
+      this.$slots.default
+    );
+  }
+});
