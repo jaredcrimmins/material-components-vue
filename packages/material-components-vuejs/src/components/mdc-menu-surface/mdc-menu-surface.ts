b91e051afbcbf76e4e932c36a6cc290439b42b33
Jared Crimmins <jared@crimmins.io>
2021-10-06 02:07:31 -0500

    refactor(menu,menu-surface): remove "anchorElement" prop validator

--- packages/material-components-vuejs/src/components/mdc-menu-surface/mdc-menu-surface.ts
+++ packages/material-components-vuejs/src/components/mdc-menu-surface/mdc-menu-surface.ts
@@ -17,10 +17,7 @@ export default Vue.extend({
       default: Corner.TOP_LEFT
     },
     anchorElement: {
-      type: <PropType<Element |string | null>>[String, Element],
-      validator: value => {
-        return ["string", "object"].includes(typeof value);
-      },
+      type: <PropType<Element | HTMLElement |string | null>>[String, Element],
       default: null
     },
     fixedPosition: Boolean,
