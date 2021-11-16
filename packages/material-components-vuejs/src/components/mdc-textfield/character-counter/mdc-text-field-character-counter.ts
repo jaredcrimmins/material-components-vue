2d1ddcf3be963b1fba2df1a9fee266f6fa1b7a84
Jared Crimmins <jared@crimmins.io>
2021-11-15 23:01:02 -0600

    refactor(textfield): rewrite in TypeScript

---
+++ packages/material-components-vuejs/src/components/mdc-textfield/character-counter/mdc-text-field-character-counter.ts
@@ -0,0 +1,86 @@
+import {MDCTextFieldCharacterCounterFoundation} from "@material/textfield/character-counter";
+import Vue, {VNode} from 'vue';
+
+export default Vue.extend({
+  name: "mdc-text-field-character-counter",
+
+  data() {
+    return {
+      content: ""
+    };
+  },
+
+  props: {
+    currentLength: {
+      default: 0,
+      type: Number
+    },
+    maxLength: {
+      default: 0,
+      type: Number
+    }
+  },
+
+  watch: {
+    currentLength() {
+      this.genContentString();
+    },
+
+    maxLength() {
+      this.genContentString();
+    }
+  },
+
+  mounted() {
+    this.init();
+  },
+
+  beforeDestroy() {
+    this.deinit();
+  },
+
+  render(c): VNode {
+    return c(
+      "div",
+      {
+        staticClass: "mdc-text-field-character-counter"
+      },
+      this.content
+    );
+  },
+
+  methods: {
+    init() {
+      this.mdcFoundation = new MDCTextFieldCharacterCounterFoundation(this);
+      this.mdcFoundation.init();
+
+      this.genContentString();
+    },
+
+    deinit() {
+      this.mdcFoundation.destroy();
+    },
+
+    genContentString(currentLength?: number, maxLength?: number) {
+      let content = "";
+
+      currentLength = currentLength || this.currentLength;
+      maxLength = maxLength || this.maxLength;
+
+      content = `${ currentLength }`;
+
+      if(maxLength) {
+        content += ` / ${ maxLength }`;
+      }
+
+      this.content = content;
+
+      return content;
+    },
+
+    // Adapter methods
+    setContent(content: string) {
+      this.content = content;
+    }
+  }
+});
---
+++ packages/material-components-vuejs/src/components/mdc-textfield/helper-text/mdc-text-field-helper-text.ts
@@ -0,0 +1,78 @@
+import {MDCTextFieldHelperTextFoundation} from "@material/textfield/helper-text";
+import Vue, {VNode} from 'vue';
+
+export default Vue.extend({
+    name: "mdc-text-field-helper-text",
+
+    data() {
+        return {
+            content_: this.content
+        };
+    },
+
+    props: {
+        content: {
+            default: () => (""),
+            type: String
+        },
+        persistent: {
+            default: false,
+            type: Boolean
+        }
+    },
+
+    mounted() {
+        this.mdcFoundation = new MDCTextFieldHelperTextFoundation(this);
+        this.mdcFoundation.init();
+        this.mdcFoundation.setValidation(true);
+    },
+
+    render(c): VNode {
+        return c(
+            "div",
+            {
+                staticClass: "mdc-text-field-helper-text",
+                class: {
+                    "mdc-text-field-helper-text--persistent": this.persistent
+                },
+                attrs: {
+                    "aria-hidden": true
+                }
+            },
+            [
+                this.content
+            ]
+        );
+    },
+
+    methods: {
+        addClass(className: string) {
+            this.$el.classList.add(className);
+        },
+
+        hasClass(className: string) {
+            return this.$el.classList.contains(className);
+        },
+
+        removeAttr(attr: string) {
+            this.$el.setAttribute(attr, "");
+        },
+
+        removeClass(className: string) {
+            this.$el.classList.remove(className);
+        },
+
+        setAttr(attr: string, value: string) {
+            this.$el.setAttribute(attr, value)
+        },
+
+        setContent(attr: string) {
+            this.content = attr;
+        },
+
+        // Foundation proxy methods
+        setValidity(inputIsValid: boolean) {
+            this.mdcFoundation.setValidity(inputIsValid);
+        }
+    }
+});
