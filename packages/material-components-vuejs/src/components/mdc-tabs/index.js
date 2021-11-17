9941d3c8414a9e88c057908d8e9efcecab8abe61
Jared Crimmins <jared@crimmins.io>
2021-11-17 15:51:56 -0600

    refactor(tabs): rewrite in TypeScript

--- packages/material-components-vuejs/src/components/mdc-tabs/index.js
+++
@@ -1,4 +0,0 @@
-export {default as MDCTab} from "./tab";
-export {default as MDCTabBar} from "./tab-bar";
-export {default as MDCTabIndicator} from "./tab-indicator";
-export {default as MDCTabScroller} from "./tab-scroller";
---
+++ packages/material-components-vuejs/src/components/mdc-tabs/tab-scroller.ts
@@ -0,0 +1,171 @@
+import {MDCTabScrollerFoundation} from "@material/tab-scroller";
+import Vue, {VNode} from 'vue';
+import {matches} from "@material/dom/ponyfill";
+import * as util from "@material/tab-scroller/util";
+
+type ScrollAreaElRef = HTMLElement;
+type ScrollContentElRef = HTMLElement;
+
+export default Vue.extend({
+  name: "mdc-tab-scroller",
+
+  props: {
+    alignStart: Boolean,
+    alignEnd: Boolean,
+    alignCenter: Boolean
+  },
+
+  data() {
+    return {
+      mdcFoundation: new MDCTabScrollerFoundation(
+        MDCTabScrollerFoundation.defaultAdapter
+      )
+    };
+  },
+
+  render(c): VNode {
+    return c(
+      "div",
+      {
+        staticClass: "mdc-tab-scroller",
+        class: {
+          "mdc-tab-scroller--align-start": this.alignStart,
+          "mdc-tab-scroller--align-end": this.alignEnd,
+          "mdc-tab-scroller--align-center": this.alignCenter
+        }
+      },
+      [
+        c(
+          "div",
+          {
+            ref: "scrollAreaEl",
+            staticClass: "mdc-tab-scroller__scroll-area",
+            on: {
+              wheel: (event: WheelEvent) => {
+                this.mdcFoundation.handleInteraction(event);
+              },
+              touchstart: (event: TouchEvent) => {
+                this.mdcFoundation.handleInteraction(event);
+              },
+              pointerdown: (event: PointerEvent) => {
+                this.mdcFoundation.handleInteraction(event);
+              },
+              mousedown: (event: MouseEvent) => {
+                this.mdcFoundation.handleInteraction(event);
+              },
+              keydown: (event: KeyboardEvent) => {
+                this.mdcFoundation.handleInteraction(event);
+              }
+            }
+          },
+          [
+            c(
+              "div",
+              {
+                ref: "scrollContentEl",
+                staticClass: "mdc-tab-scroller__scroll-content",
+                on: {
+                  transitionend: (event: TransitionEvent) => {
+                    this.mdcFoundation.handleTransitionEnd(event);
+                  }
+                }
+              },
+              this.$slots.default
+            )
+          ]
+        )
+      ]
+    );
+  },
+
+  mounted() {
+    this.mdcFoundation = new MDCTabScrollerFoundation(this);
+    this.mdcFoundation.init();
+  },
+
+  beforeDestroy() {
+    this.mdcFoundation.destroy();
+  },
+
+  methods: {
+    //
+    // Public methods
+    //
+
+    scrollTo(scrollX: number) {
+      this.mdcFoundation.scrollTo(scrollX);
+    },
+
+    incrementScroll(scrollXIncrement: number) {
+      this.mdcFoundation.incrementScroll(scrollXIncrement);
+    },
+
+    getScrollPosition() {
+      return this.mdcFoundation.getScrollPosition();
+    },
+
+    getScrollContentWidth() {
+      return (<ScrollContentElRef>this.$refs.scrollContentEl).offsetWidth;
+    },
+
+    //
+    // Adapter methods
+    //
+
+    eventTargetMatchesSelector(eventTarget: EventTarget, selector: string) {
+      return matches(<Element>eventTarget, selector);
+    },
+
+    addClass(className: string) {
+      this.$el.classList.add(className);
+    },
+
+    addScrollAreaClass(className: string) {
+      (<ScrollAreaElRef>this.$refs.scrollAreaEl).classList.add(className);
+    },
+
+    removeClass(className: string) {
+      this.$el.classList.remove(className);
+    },
+
+    setScrollAreaStyleProperty(property: string, value: string) {
+      (<ScrollAreaElRef>this.$refs.scrollAreaEl).style.setProperty(property, value);
+    },
+
+    setScrollContentStyleProperty(property: string, value: string) {
+      (<ScrollContentElRef>this.$refs.scrollContentEl).style.setProperty(property, value);
+    },
+
+    getScrollContentStyleValue(property: string) {
+      return window.getComputedStyle((<ScrollContentElRef>this.$refs.scrollContentEl)).getPropertyValue(property);
+    },
+
+    setScrollAreaScrollLeft(scrollLeft: number) {
+      (<ScrollAreaElRef>this.$refs.scrollAreaEl).scrollLeft = scrollLeft;
+    },
+
+    getScrollAreaScrollLeft() {
+      return (<ScrollAreaElRef>this.$refs.scrollAreaEl).scrollLeft;
+    },
+
+    getScrollContentOffsetWidth() {
+      return (<ScrollContentElRef>this.$refs.scrollContentEl).offsetWidth;
+    },
+
+    getScrollAreaOffsetWidth() {
+      return (<ScrollAreaElRef>this.$refs.scrollAreaEl).offsetWidth;
+    },
+
+    computeScrollAreaClientRect() {
+      return (<ScrollAreaElRef>this.$refs.scrollAreaEl).getBoundingClientRect();
+    },
+
+    computeScrollContentClientRect() {
+      return (<ScrollContentElRef>this.$refs.scrollContentEl).getBoundingClientRect();
+    },
+
+    computeHorizontalScrollbarHeight() {
+      return util.computeHorizontalScrollbarHeight(document);
+    }
+  }
+});
