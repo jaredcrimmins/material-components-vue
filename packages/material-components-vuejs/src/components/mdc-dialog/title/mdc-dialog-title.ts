3197970c3ea88201dcc7fcb2c90f17d92bcdaa51
Jared Crimmins <jared@crimmins.io>
2021-11-17 14:57:34 -0600

    refactor(dialog): rewrite in TypeScript

--- packages/material-components-vuejs/src/components/mdc-dialog/title/mdc-dialog-title.ts
+++ packages/material-components-vuejs/src/components/mdc-dialog/title/mdc-dialog-title.ts
@@ -1,11 +1,17 @@
-export default {
+import Vue, {VueConstructor, VNode} from 'vue';
+
+interface Injections {
+  mdcDialogTitleID__: string
+}
+
+export default (<VueConstructor<Vue & Injections>>Vue).extend({
   name: 'mdc-dialog-title',
 
   inheritAttrs: true,
 
   inject: ['mdcDialogTitleID__'],
 
-  render(c) {
+  render(c): VNode | void {
     if (!this.$slots.default) return;
 
     return c(
@@ -19,4 +25,4 @@ export default {
       this.$slots.default
     );
   }
-}
+});
