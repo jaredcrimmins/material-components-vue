304245bbbb94189a3997dddb8213bb4529e3f398
Jared Crimmins <jared@crimmins.io>
2022-01-26 19:24:28 -0600

    build: add rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,59 @@
+import typescript from '@rollup/plugin-typescript';
+
+const input = 'src/index.ts';
+const plugins = [typescript()];
+const external = [
+  '@material/animation/util',
+  '@material/checkbox',
+  '@material/circular-progress',
+  '@material/dialog',
+  '@material/dom/events',
+  '@material/dom/focus-trap',
+  '@material/dom/ponyfill',
+  '@material/floating-label',
+  '@material/icon-button',
+  '@material/linear-progress',
+  '@material/line-ripple',
+  '@material/list',
+  '@material/menu',
+  '@material/menu/constants',
+  '@material/menu-surface',
+  '@material/notched-outline',
+  '@material/radio',
+  '@material/ripple',
+  '@material/snackbar',
+  '@material/select',
+  '@material/switch',
+  '@material/tab',
+  '@material/tab-bar',
+  '@material/tab-indicator',
+  '@material/tab-scroller',
+  '@material/tab-scroller/util',
+  '@material/textfield',
+  '@material/textfield/character-counter',
+  '@material/textfield/helper-text',
+  'vue'
+];
+
+export default [
+  {
+    input,
+    output: {
+      file: 'dist/material-components-vuejs.esm.js',
+      format: 'es',
+      sourcemap: true
+    },
+    plugins,
+    external
+  },
+  {
+    input,
+    output: {
+      file: 'dist/material-components-vuejs.common.js',
+      format: 'cjs',
+      sourcemap: true
+    },
+    plugins,
+    external
+  }
+];
