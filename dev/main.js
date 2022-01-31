9ea11160af533fa12a8de2338e4e7779bcf5f772
Jared Crimmins <jared@crimmins.io>
2022-01-31 13:55:54 -0600

    build(dev-app): import plugin using the `MaterialComponentsVuejs` export

--- dev/main.js
+++ dev/main.js
@@ -1,12 +1,12 @@
 import App from "./app.vue";
-import MaterialComponentsVue from "./../../material-components-vuejs/src/index";
+import {MaterialComponentsVuejs} from "./../../material-components-vuejs/src/index";
 import {VDebugContent} from "./components";
 import Vue from "vue";
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
 
-Vue.use(MaterialComponentsVue);
+Vue.use(MaterialComponentsVuejs);
 Vue.component("v-debug-content", VDebugContent);
 
 export function createApp() {
