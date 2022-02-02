6def10b3ab1b259b3805a3dccb76bae27c8436e4
Jared Crimmins <jared@crimmins.io>
2022-02-02 15:46:08 -0600

    build(rollup): replace @material/* external module IDs with regex

--- rollup.config.js
+++ rollup.config.js
@@ -3,29 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 const input = 'src/index.ts';
 const plugins = [typescript()];
 const external = [
-  '@material/animation',
-  '@material/checkbox',
-  '@material/circular-progress',
-  '@material/dialog',
-  '@material/dom',
-  '@material/floating-label',
-  '@material/icon-button',
-  '@material/linear-progress',
-  '@material/line-ripple',
-  '@material/list',
-  '@material/menu',
-  '@material/menu-surface',
-  '@material/notched-outline',
-  '@material/radio',
-  '@material/ripple',
-  '@material/snackbar',
-  '@material/select',
-  '@material/switch',
-  '@material/tab',
-  '@material/tab-bar',
-  '@material/tab-indicator',
-  '@material/tab-scroller',
-  '@material/textfield',
+  /@material/,
   'vue'
 ];
 
