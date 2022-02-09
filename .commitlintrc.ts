7beb5094dd8b0fb8f54b3de99cd73ac4e8b15d93
Jared Crimmins <jared@crimmins.io>
2022-02-09 03:58:29 -0600

    chore: configure commitlint

---
+++ .commitlintrc.ts
@@ -0,0 +1,66 @@
+import type {UserConfig} from '@commitlint/types';
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [2, 'always', 72],
+    'header-max-length': [2, 'always', 72],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'release',
+        'revert',
+        'style',
+        'test'
+      ]
+    ],
+    'scope-enum': [
+      1,
+      'always',
+      [
+        // Components
+        'multiple', // For when a commit applies to multiple components.
+        'button',
+        'checkbox',
+        'circular-progress',
+        'dialog',
+        'floating-label',
+        'icon-button',
+        'image-list',
+        'line-ripple',
+        'linear-progress',
+        'list',
+        'material-icon',
+        'menu',
+        'menu-surface',
+        'notched-outline',
+        'radio',
+        'ripple',
+        'select',
+        'snackbar',
+        'switch',
+        'tabs',
+        'textfield',
+
+        // Mixins
+        'absolutely-positionable',
+
+        // Dev app
+        'dev',
+
+        // Build
+        'rollup'
+      ]
+    ]
+  }
+}
+
+export default Configuration;
