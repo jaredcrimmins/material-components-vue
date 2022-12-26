b2743197e1244dc632166ae23836a3c37611afea
Jared Crimmins <jared@crimmins.io>
2022-12-26 15:45:59 -0600

    style(textfield): modify `MDCTextFieldHelperText` component file
    Convert `MDCTextFieldHelperText` component definition file to 2 space
    indentation and replace `"` with `'`.

--- src/components/mdc-textfield/helper-text/mdc-text-field-helper-text.ts
+++ src/components/mdc-textfield/helper-text/mdc-text-field-helper-text.ts
@@ -1,87 +1,87 @@
-import {MDCTextFieldHelperTextFoundation} from "@material/textfield";
+import {MDCTextFieldHelperTextFoundation} from '@material/textfield';
 import Vue, {VNode} from 'vue';
 
 export default Vue.extend({
-    name: "mdc-text-field-helper-text",
+  name: 'mdc-text-field-helper-text',
 
-    data() {
-        return {
-            content_: this.content,
-            mdcFoundation: new MDCTextFieldHelperTextFoundation(
-              MDCTextFieldHelperTextFoundation.defaultAdapter
-            )
-        };
+  data() {
+    return {
+      content_: this.content,
+      mdcFoundation: new MDCTextFieldHelperTextFoundation(
+        MDCTextFieldHelperTextFoundation.defaultAdapter
+      )
+    };
+  },
+
+  props: {
+    content: {
+      default: () => (''),
+      type: String
     },
-
-    props: {
-        content: {
-            default: () => (""),
-            type: String
-        },
-        persistent: {
-            default: false,
-            type: Boolean
-        }
-    },
-
-    watch: {
-      content(value) {
-        this.content_ = value;
-      }
-    },
-
-    mounted() {
-        this.mdcFoundation = new MDCTextFieldHelperTextFoundation(this);
-        this.mdcFoundation.init();
-        this.mdcFoundation.setValidation(true);
-    },
-
-    render(c): VNode {
-        return c(
-            "div",
-            {
-                staticClass: "mdc-text-field-helper-text",
-                class: {
-                    "mdc-text-field-helper-text--persistent": this.persistent
-                },
-                attrs: {
-                    "aria-hidden": true
-                }
-            },
-            [
-                this.content_
-            ]
-        );
-    },
-
-    methods: {
-        addClass(className: string) {
-            this.$el.classList.add(className);
-        },
-
-        hasClass(className: string) {
-            return this.$el.classList.contains(className);
-        },
-
-        removeAttr(attr: string) {
-            this.$el.setAttribute(attr, "");
-        },
-
-        removeClass(className: string) {
-            this.$el.classList.remove(className);
-        },
-
-        setAttr(attr: string, value: string) {
-            this.$el.setAttribute(attr, value)
-        },
-
-        setContent(attr: string) {
-            this.content_ = attr;
-        },
-
-        // Foundation proxy methods
-        setValidity(inputIsValid: boolean) {
-            this.mdcFoundation.setValidity(inputIsValid);
-        }
+    persistent: {
+      default: false,
+      type: Boolean
     }
+  },
+
+  watch: {
+    content(value) {
+      this.content_ = value;
+    }
+  },
+
+  mounted() {
+    this.mdcFoundation = new MDCTextFieldHelperTextFoundation(this);
+    this.mdcFoundation.init();
+    this.mdcFoundation.setValidation(true);
+  },
+
+  render(c): VNode {
+    return c(
+      'div',
+      {
+        staticClass: 'mdc-text-field-helper-text',
+        class: {
+          'mdc-text-field-helper-text--persistent': this.persistent
+        },
+        attrs: {
+          'aria-hidden': true
+        }
+      },
+      [
+        this.content_
+      ]
+    );
+  },
+
+  methods: {
+    addClass(className: string) {
+      this.$el.classList.add(className);
+    },
+
+    hasClass(className: string) {
+      return this.$el.classList.contains(className);
+    },
+
+    removeAttr(attr: string) {
+      this.$el.setAttribute(attr, '');
+    },
+
+    removeClass(className: string) {
+      this.$el.classList.remove(className);
+    },
+
+    setAttr(attr: string, value: string) {
+      this.$el.setAttribute(attr, value)
+    },
+
+    setContent(attr: string) {
+      this.content_ = attr;
+    },
+
+    // Foundation proxy methods
+    setValidity(inputIsValid: boolean) {
+      this.mdcFoundation.setValidity(inputIsValid);
+    }
+  }
 });
