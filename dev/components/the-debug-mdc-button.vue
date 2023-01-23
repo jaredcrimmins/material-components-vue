26c0621a0c613db7898a046fcf651533d2490592
Jared Crimmins <jared@crimmins.io>
2023-01-23 13:18:26 -0600

    chore(dev): add additional examples to `TheDebugMdcButton` component

--- dev/components/the-debug-mdc-button.vue
+++ dev/components/the-debug-mdc-button.vue
@@ -4,7 +4,54 @@
       MDCButton
     </h2>
 
+    <h3>Text button</h3>
+
     <mdc-button>MDCButton</mdc-button>
+
+    <h4>Text Button plus icon</h4>
+
+    <mdc-button>
+      <template #append>
+        <mdc-material-icon>bookmark</mdc-material-icon>
+      </template>
+      <template #default>
+        Text Button plus icon
+      </template>
+    </mdc-button>
+
+    <h3>Outlined button</h3>
+
+    <mdc-button outlined>
+      Outlined Button
+    </mdc-button>
+
+    <h4>Outlined button with icon</h4>
+
+    <mdc-button outlined>
+      <template #append>
+        <mdc-material-icon>bookmark</mdc-material-icon>
+      </template>
+      <template #default>
+        Outlined Button plus icon
+      </template>
+    </mdc-button>
+
+    <h3>Contained button</h3>
+
+    <mdc-button raised>
+      Contained Button
+    </mdc-button>
+
+    <h4>Contained button with icon</h4>
+
+    <mdc-button raised>
+      <template #append>
+        <mdc-material-icon>bookmark</mdc-material-icon>
+      </template>
+      <template #default>
+        Contained Button plus icon
+      </template>
+    </mdc-button>
   </div>
 </template>
 
