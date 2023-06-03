9920d615067977e7c08de04aa2e795599c1d971d
Jared Crimmins <jared@crimmins.io>
2023-06-02 23:24:56 -0500

    fix(menu-surface): add/remove classes using a Vue mechanism

--- src/components/mdc-menu-surface/mdc-menu-surface.ts
+++ src/components/mdc-menu-surface/mdc-menu-surface.ts
@@ -33,6 +33,9 @@ export default Vue.extend({
   data() {
     return {
       anchorElement_: <Element | HTMLElement | null>null,
+      cssClasses: {
+        'mdc-menu-surface--fullwidth': this.fullWidth
+      } as {[className: string]: boolean},
       mdcFoundation: new MDCMenuSurfaceFoundation(MDCMenuSurfaceFoundation.defaultAdapter),
       open: false,
       previousFocus: <HTMLElement | null>null
@@ -92,9 +95,7 @@ export default Vue.extend({
       'div',
       {
         staticClass: cssClasses.ROOT,
-        class: {
-          'mdc-menu-surface--fullwidth': this.fullWidth
-        },
+        class: this.cssClasses,
         on: {
           keydown: this.onKeydown,
           'MDCMenuSurface:closed': this.onClosed,
@@ -206,15 +207,15 @@ export default Vue.extend({
     //
 
     addClass(className: string) {
-      this.$el.classList.add(className);
+      this.cssClasses = {...this.cssClasses, [className]: true};
     },
 
     removeClass(className: string) {
-      this.$el.classList.remove(className);
+      this.cssClasses = {...this.cssClasses, [className]: false};
     },
 
     hasClass(className: string) {
-      return this.$el.classList.contains(className);
+      return !!this.cssClasses[className];
     },
 
     hasAnchor() {
