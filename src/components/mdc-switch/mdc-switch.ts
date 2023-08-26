3e93bf7a3f6f28bb28fb7684df954c654ee0bad0
Jared Crimmins <jared@crimmins.io>
2023-08-26 15:45:18 -0500

    style(switch): convert definition file to 2 spaces indentation

--- src/components/mdc-switch/mdc-switch.ts
+++ src/components/mdc-switch/mdc-switch.ts
@@ -2,71 +2,71 @@ import {MDCSwitch} from "@material/switch";
 import Vue, {CreateElement, VNode} from 'vue';
 
 export default Vue.extend({
-    name: "mdc-switch",
+  name: "mdc-switch",
 
-    props: {
-        checked: Boolean
-    },
+  props: {
+    checked: Boolean
+  },
 
-    data() {
-        return <{mdcTextField: null | InstanceType<typeof MDCSwitch>}>{
-            mdcTextField: null
-        };
-    },
+  data() {
+    return <{mdcTextField: null | InstanceType<typeof MDCSwitch>}>{
+      mdcTextField: null
+    };
+  },
 
-    mounted() {
-        this.mdcTextField = new MDCSwitch(this.$el);
-    },
+  mounted() {
+    this.mdcTextField = new MDCSwitch(this.$el);
+  },
 
-    render(c): VNode {
-        return c(
+  render(c): VNode {
+    return c(
+      "div",
+      {
+        staticClass: "mdc-switch"
+      },
+      [
+        c(
+          "div",
+          {
+            staticClass: "mdc-switch__track"
+          },
+          [
+            this.genThumbUnderlay(c)
+          ]
+        )
+      ]
+    );
+  },
+
+  methods: {
+    genThumbUnderlay(c: CreateElement) {
+      return c(
+        "div",
+        {
+          staticClass: "mdc-switch__thumb-underlay"
+        },
+        [
+          c(
             "div",
             {
-                staticClass: "mdc-switch"
-            },
-            [
-                c(
-                    "div",
-                    {
-                        staticClass: "mdc-switch__track"
-                    },
-                    [
-                      this.genThumbUnderlay(c)
-                    ]
-                )
-            ]
-        );
-    },
-
-    methods: {
-        genThumbUnderlay(c: CreateElement) {
-            return c(
-                "div",
-                {
-                    staticClass: "mdc-switch__thumb-underlay"
-                },
-                [
-                    c(
-                        "div",
-                        {
-                            staticClass: "mdc-switch__thumb"
-                        }
-                    ),
-                    c(
-                        "input",
-                        {
-                            staticClass: "mdc-switch__native-control",
-                            attrs: {
-                                "type": "checkbox",
-                                "role": "switch"
-                            },
-                            props: {
-                                value: this.checked
-                            }
-                        }
-                    )
-                ]
-            );
-        }
+              staticClass: "mdc-switch__thumb"
+            }
+          ),
+          c(
+            "input",
+            {
+              staticClass: "mdc-switch__native-control",
+              attrs: {
+                "type": "checkbox",
+                "role": "switch"
+              },
+              props: {
+                value: this.checked
+              }
+            }
+          )
+        ]
+      );
     }
+  }
 });
