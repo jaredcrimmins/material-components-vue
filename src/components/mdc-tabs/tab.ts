32d6ac461b7e493b17100ebe1d2438984290d225
Jared Crimmins <jared@crimmins.io>
2023-12-18 22:34:08 -0600

    fix(tabs): fix issue that prevented classes from being prg'ly removed

--- src/components/mdc-tabs/tab.ts
+++ src/components/mdc-tabs/tab.ts
@@ -79,6 +79,7 @@ export default (<VueConstructor<Vue & Injections>>Vue).extend({
       "button",
       {
         staticClass: "mdc-tab",
+        class: this.cssClasses,
         attrs: {
           id: this.id,
           role: "tab"
